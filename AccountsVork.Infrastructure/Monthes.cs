5f7bd0cca412a266843082a5cc73e4d5796470ce
Роман <fredoqw@outlook.com>
2016-05-03 10:11:05 +0300

    nh

---
+++ AccountsVork.Infrastructure/Monthes.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace AccountsVork.Infrastructure
+{
+    public static class Monthes
+    { 
+        public static List<string> GetMonthesList()
+        {
+            { }
+            ///here///
+            return new List<string>()
+            {
+                "Январь","Февраль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь"
+            };
+        }
+    }
+}
